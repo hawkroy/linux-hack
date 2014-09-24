00318592bbd65e7daa3f4f31684500a6e3e68662
Wang,Zheng <zheng1.wang@intel.com>
2014-09-24 18:01:23 +0800

    modify test script

--- test/start_debug.sh
+++ test/start_debug.sh
@@ -1,2 +1,2 @@
 #!/bin/sh 
-/home/zhengwa1/opt/qemu-1.7.1/bin/qemu-system-i386 -fda $1 -fdb rootimage-0.11-my -s -S -monitor stdio -debugcon file:debug.log -global isa-debugcon.iobase=0x402
+/home/zhengwa1/opt/qemu-1.7.1/bin/qemu-system-i386 -fda $1 -fdb $2 -s -S -monitor stdio -debugcon file:debug.log -global isa-debugcon.iobase=0x402
